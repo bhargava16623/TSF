608ab856493d70cd928e01f509408952b23474cc
bhargava16623 <105556187+bhargava16623@users.noreply.github.com>
2024-03-19 19:50:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var elemC= document.querySelector("#elem-container");
+var fixed= document.querySelector("#fixed-img");
+
+elemC.addEventListener("mouseenter", function(){
+    fixed.style.display="block";
+});
+
+elemC.addEventListener("mouseleave", function(){
+    fixed.style.display="none";
+});
+{/*
+var elem1= document.querySelector("#elem1");
+elem1.addEventListener("mouseenter", function(){
+    var image=elem1.getAttribute("data-image");
+    console.log(image);
+    fixed.style.backgroundImage= `url(${image})`;
+});*/
+}
+
+var elems= document.querySelectorAll(".elem");
+
+console.log(elems);
+
+elems.forEach(function(e){
+    e.addEventListener("mouseenter", function(){
+        var image = e.getAttribute("data-image");
+        fixed.style.backgroundImage= `url(${image})`;
+    });
+})
